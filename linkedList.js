bf6a68009d3732f3440dc338f457dbb045268607
Elijah Williams <ejwill04@gmail.com>
2016-10-19 09:05:46 -0600

    Add CSS margins to sections, adjust positioning

--- linkedList.js
+++ linkedList.js
@@ -18,17 +18,4 @@ $('.bookmarks').append('<div class="bookmark-card">' +
   '<input id="bookmark-delete-button" type="button" name="Delete" value="Delete">' +
   '</div>'
 );
-}
-//   var websiteTitleValue = $('#website-title').val();
-//   var websiteUrlField = $('#website-url').val();
-//   $('.bookmarks').append('<div>websiteTitleValue</div>');
-//   $('.bookmarks').append('<div>websiteUrlField</div>');
-// });
-
-// function displayBookmarkInfo(){
-//   $('.bookmark-title').text(websiteTitleValue);
-//   $('.bookmark-url').text(websiteUrlField);
-// }
-
-//read on click
->>>>>>> master
+} 
