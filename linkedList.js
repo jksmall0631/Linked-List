c250110202aec63fdb357e632b4ad6922fde558e
Joshua Small <jksmall0631@gmail.com>
2016-10-18 18:07:14 -0600

    make button and display function

--- linkedList.js
+++ linkedList.js
@@ -2,5 +2,20 @@ var websiteTitleField = $('#website-title').value;
 var websiteUrlField = $('#website-url').value;
 
 $('#enter-button').on('click', function(){
-
+  displayBookmarkInfo();
 });
+
+function displayBookmarkInfo(){
+  var websiteTitleValue = $('#website-title').val();
+  var websiteUrlField = $('#website-url').value;
+  $('#bookmark-title').text(websiteTitleValue);
+  $('#bookmark-url').text(websiteUrlField);
+}
+
+
+  // $('#enter-button').on("click", function() {
+  //   var replace = $('h2').replaceWith(function() {
+  //     return $(this).contents();
+// 
+//   });
+// });
