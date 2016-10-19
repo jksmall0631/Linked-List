90f44e080c22d972a633b04eade0762e18a66808
Elijah Williams <ejwill04@gmail.com>
2016-10-18 19:58:02 -0600

    Try to add HTML tags tp displayBookmarkInfo

--- linkedList.js
+++ linkedList.js
@@ -1,21 +1,15 @@
-var websiteTitleField = $('#website-title').value;
-var websiteUrlField = $('#website-url').value;
-
 $('#enter-button').on('click', function(){
-  displayBookmarkInfo();
+  // displayBookmarkInfo();
+  $(displayBookmarkInfo()).appendTo().html();
 });
 
 function displayBookmarkInfo(){
   var websiteTitleValue = $('#website-title').val();
-  var websiteUrlField = $('#website-url').value;
-  $('#bookmark-title').text(websiteTitleValue);
-  $('#bookmark-url').text(websiteUrlField);
+  var websiteUrlField = $('#website-url').val();
+  $('.bookmark-title').text(websiteTitleValue);
+  $('.bookmark-url').text(websiteUrlField);
 }
 
+//read on click
 
-  // $('#enter-button').on("click", function() {
-  //   var replace = $('h2').replaceWith(function() {
-  //     return $(this).contents();
-// 
-//   });
-// });
+//delete on click
