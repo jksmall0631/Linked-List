b21b067119e5caaec73f905463a7f0a9f023eaeb
Elijah Williams <ejwill04@gmail.com>
2016-10-18 22:13:57 -0600

    Initial CSS to bookmark-card and website-title

--- linkedList.js
+++ linkedList.js
@@ -1,15 +1,13 @@
 $('#enter-button').on('click', function(){
-
-  displayBookmarkInfo();
-  $('.bookmarks').append('<div>bla</div>');
-
-});
-
-function displayBookmarkInfo(){
   var websiteTitleValue = $('#website-title').val();
   var websiteUrlField = $('#website-url').val();
-  $('.bookmark-title').text(websiteTitleValue);
-  $('.bookmark-url').text(websiteUrlField);
-}
+  $('.bookmarks').append('<div>websiteTitleValue</div>');
+  $('.bookmarks').append('<div>websiteUrlField</div>');
+});
+
+// function displayBookmarkInfo(){
+//   $('.bookmark-title').text(websiteTitleValue);
+//   $('.bookmark-url').text(websiteUrlField);
+// }
 
 //read on click
