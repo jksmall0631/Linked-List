d2592572bbda64663fe7ece7ae4876670f864929
Elijah Williams <ejwill04@gmail.com>
2016-10-19 15:05:00 -0600

    Add delete button functionality

--- linkedList.js
+++ linkedList.js
@@ -2,28 +2,40 @@ $('#enter-button').on('click', function(){
   createCard();
 });
 
-$('#bookmark-delete-button').on('click', function(){
-  deleteFunction();
-});
-
 function createCard(){
-  var websiteTitleValue = $('#website-title').val();
-  var websiteUrlField = $('#website-url').val();
+var websiteTitleValue = $('#website-title').val();
+var websiteUrlField = $('#website-url').val();
+
 $('.bookmarks').append('<div class="bookmark-card">' +
   '<h1 class="bookmark-title">' + websiteTitleValue + '</h1>' +
   '<a href="#"><h2 class="bookmark-url">' + websiteUrlField + '</h2></a>' +
-  '<input id="bookmark-read-button" type="button" name="Read" value="Read">' +
-  '<input id="bookmark-delete-button" type="button" name="Delete" value="Delete">' +
+  '<input class="bookmark-read-button" type="button" name="Read" value="Read">' +
+  '<input class="bookmark-delete-button" type="button" name="Delete" value="Delete">' +
   '</div>'
 );
+
+$('.bookmark-delete-button').on('click', function(){
+  $(this).parent().remove();
+});
+
+$('.bookmark-read-button').on('click', function(){
+  $(this).toggleClass("red");
+});
 }
 
-function readButton(){
-  this.button = $('#bookmark-read-button');
-  this.button.toggleClass('.read');
-}
+// function readButton(){
+//   this.button = $('.bookmark-read-button');
+//   this.button.toggleClass('.read');
+// }
 
-function deleteFunction(){
-  $('bookmark-card').remove();
+// deleteFunction();
+// function deleteFunction(){
   // this.button = $('#bookmark-delete-button');
-}
+
+  // $('.bookmarks').on('click',$('.bookmark-delete-button'), function(){
+  //   deleteFunction();
+  // });
+
+  // $('.bookmark-delete-button').on('click', function(){
+  //   deleteFunction();
+  // });
