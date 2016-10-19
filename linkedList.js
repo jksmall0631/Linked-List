8f774daefce084c57a5e149c521b3aa307288217
Joshua Small <jksmall0631@gmail.com>
2016-10-18 18:53:50 -0600

    add append functionality

--- linkedList.js
+++ linkedList.js
@@ -3,19 +3,21 @@ var websiteUrlField = $('#website-url').value;
 
 $('#enter-button').on('click', function(){
   displayBookmarkInfo();
+  $('.bookmark-card').append('<div>bla</div>');
 });
 
 function displayBookmarkInfo(){
   var websiteTitleValue = $('#website-title').val();
-  var websiteUrlField = $('#website-url').value;
-  $('#bookmark-title').text(websiteTitleValue);
-  $('#bookmark-url').text(websiteUrlField);
+  var websiteUrlField = $('#website-url').val();
+  $('.bookmark-title').text(websiteTitleValue);
+  $('.bookmark-url').text(websiteUrlField);
 }
+
 
 
   // $('#enter-button').on("click", function() {
   //   var replace = $('h2').replaceWith(function() {
   //     return $(this).contents();
-// 
+//
 //   });
 // });
