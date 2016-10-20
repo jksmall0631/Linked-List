1e1820c624a25a676f8f92b272dc212381ca0601
Elijah Williams <ejwill04@gmail.com>
2016-10-19 18:38:04 -0600

    Progress on count cards

--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,10 @@
 $('#enter-button').on('click', function(){
   createCard();
+  countBookmarkCards();
+  countUnreadCards();
 });
+
+
 
 function createCard(){
 var websiteTitleValue = $('#website-title').val();
@@ -10,38 +14,32 @@ $('.bookmarks').append('<div class="bookmark-card">' +
   '<h1 class="bookmark-title">' + websiteTitleValue + '</h1>' +
   '<a href="#"><h2 class="bookmark-url">' + websiteUrlField + '</h2></a>' +
   '<button class="bookmark-read-button" type="button" name="Read">Read</button>' +
-  // '<input class="bookmark-read-button" type="button" name="Read" value="Read">' +
   '<button class="bookmark-delete-button" type="button" name="Delete">Delete</button>' +
-  // '<input class="bookmark-delete-button" type="button" name="Delete" value="Delete">' +
   '</div>'
 );
 
 $('.bookmark-delete-button').on('click', function(){
   $(this).parent().remove();
+  countBookmarkCards();
 });
 
 $('.bookmark-read-button').off('click').on('click', function(){
   $(this).toggleClass("red");
   $(this).parent().toggleClass("background-color");
+  countReadCards();
+  countUnreadCards();
 });
 }
 
+function countBookmarkCards() {
+  var bookmarkCount = $('.bookmark-card').length;
+  $('.bookmark-card-count').html('<p>' + bookmarkCount + '</p>');
+}
 
+function countReadCards() {
+  return $('.red').length;
+}
 
-
-// function readButton(){
-//   this.button = $('.bookmark-read-button');
-//   this.button.toggleClass('.read');
-// }
-
-// deleteFunction();
-// function deleteFunction(){
-  // this.button = $('#bookmark-delete-button');
-
-  // $('.bookmarks').on('click',$('.bookmark-delete-button'), function(){
-  //   deleteFunction();
-  // });
-
-  // $('.bookmark-delete-button').on('click', function(){
-  //   deleteFunction();
-  // });
+function countUnreadCards() {
+  console.log($('.bookmark-card').length - $('.red').length);
+}
